ee39a4014a65398abd59760e58ba09da94f38d1d
pvtrex <zhenrime@gmail.com>
2025-05-11 18:53:38 +0530

    commit

---
+++ 4.js
@@ -0,0 +1,17 @@
+// memorry - 
+// heap(for non primitive data types)
+// stack - primitive data types
+let myyoutubename="myyoutubename";
+
+let anothername= myyoutubename
+anothername="anothername";
+console.log(anothername);
+
+let userOne={
+    Email:"user@gmail.com",
+    upi:"user@123"
+}
+let userTWo =userOne 
+userTWo.Email="hitesh@gmail.com"
+log(userOne.Email);
+// userOne.Email = "
