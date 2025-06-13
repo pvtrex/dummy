0b026f11ad9bde15131daffeaf6cb4e95932b18d
pvtrex <zhenrime@gmail.com>
2025-06-13 10:17:49 +0530

    commit

---
+++ maths.js
@@ -0,0 +1,22 @@
+const score=400
+const balance = new Number (100) 
+// console.log(balance)
+// console.log(balance.toString())
+const other =23.564845
+// console.log(other.toFixed(2)) // 23.56
+const hunderder=10100000
+// console.log(hunderder.toLocaleString("en-IN")) // 10,100,000
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1) ;
+console.log(Math.floor(Math.random()*10) + 1) ;
+const min =10
+const max =20
+console.log(Math.floor(Math.random() * (max -min +1))+min);
+
+ 
+
+
+
